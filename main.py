650ba4e337f5130583d8a43259adecdb5918bf4f
Lucendayo <Lucendayo@gmail.com>
2024-06-10 18:36:05 +0400

    feat: Remove unused XML annotation files
    The XML annotation files for the images IMG_0641, IMG_0634, IMG_0636, IMG_0637, IMG_0639, IMG_0629, IMG_0633, IMG_0635, and IMG_0640 have been deleted. These files were no longer needed and were cluttering the repository. This commit removes them to improve the organization and cleanliness of the codebase.

---
+++ main.py
@@ -0,0 +1,76 @@
+import tensorflow as tf
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(1, activation='sigmoid')(x) 
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer=Adam(learning_rate=0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    'data/train',
+    target_size=(224, 224),
+    batch_size=8, #24
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    'data/validation',
+    target_size=(224, 224),
+    batch_size=2, #6
+    class_mode='binary'
+)
+
+
+checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True, mode='min')
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, mode='min')
+
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // validation_generator.batch_size,
+    epochs=50,
+    callbacks=[checkpoint, early_stopping]
+)
+
+for layer in base_model.layers[:15]:
+    layer.trainable = False
+for layer in base_model.layers[15:]:
+    layer.trainable = True
+
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
+
+history_fine = model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.samples // train_generator.batch_size,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.samples // validation_generator.batch_size,
+    epochs=50,
+    callbacks=[checkpoint, early_stopping]
+)
+
+# Do this on linux, mac or wsl to convert the model to tensorflowjs format
+# tensorflowjs_converter --input_format=keras best_model.h5 tfjs_model
