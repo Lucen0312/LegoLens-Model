6b841569dd928c99809679e0ed272936be3b18ec
Lucendayo <Lucendayo@gmail.com>
2024-06-13 16:27:22 +0400

    asdfasdf

--- test.js
+++ test.js
@@ -1,24 +1,53 @@
-const tf = require('@tensorflow/tfjs');
-const fetch = require('node-fetch');
+const tf = require('@tensorflow/tfjs-node');
+const assert = require('assert');
+const fs = require('fs');
+const parseString = require('xml2js').parseString;
+const jpeg = require('jpeg-js');
 
-// Use node-fetch in tfjs
-global.fetch = fetch;
+describe('YOLO Model', function() {
+  let model;
+  let trainData;
 
-const testModel = async () => {
-    // Set the model URL
-    const modelUrl = 'http://localhost:8080/tfjs_model/model.json';
-
+  before(async function() {
     // Load the model
-    const model = await tf.loadLayersModel(modelUrl);
+    model = await tf.loadLayersModel('file://tfjs_model/model.json');
 
-    // Create a dummy input
-    const input = tf.zeros([1, 224, 224, 3]);
+    // Load the training data
+    // Replace 'path/to/your/images' and 'path/to/your/xmls' with the paths to your image and XML files
+    const imageFiles = fs.readdirSync('data/train');
+    const xmlFiles = fs.readdirSync('data/annotations');
+
+    trainData = await Promise.all(imageFiles.map(async (file, i) => {
+    const imageData = fs.readFileSync(`data/train/${file}`);
+    const xmlData = fs.readFileSync(`data/annotations/${xmlFiles[i]}`, 'utf8');
+
+    const image = jpeg.decode(imageData, true);
+    const tensor = tf.node.decodeImage(imageData).expandDims(0);
+
+    let result;
+    parseString(xmlData, function (err, res) {
+        result = res;
+    });
+
+    // Extract bounding box information
+    const bbox = result.annotation.object[0].bndbox[0];
+    const xmin = parseInt(bbox.xmin[0]);
+    const ymin = parseInt(bbox.ymin[0]);
+    const xmax = parseInt(bbox.xmax[0]);
+    const ymax = parseInt(bbox.ymax[0]);
+
+    return { image: tensor, annotations: { xmin, ymin, xmax, ymax } };
+    }));
+
+    it('should output the correct shape', async function() {
+    // Convert the training data to a tensor
+    const input = tf.stack(trainData.map(data => data.image));
 
     // Make a prediction
     const output = model.predict(input);
 
-    // Log the output
-    output.print();
-};
-
-testModel();
+    // Check the output shape
+    // Replace [1, 13, 13, 255] with the expected output shape of your model
+    assert.deepStrictEqual(output.shape, [1, 13, 13, 255]);
+    });
+});});
